3c294ca8498f7d3057a860a7496248f3a49fa4b1
Alesia <andomyel@yandex.ru>
2023-01-23 23:50:17 +0300

    Добавила логи, исправила запрсы чисел для степеней и корней

--- calc_operations.py
+++ calc_operations.py
@@ -1,44 +1,52 @@
 import user_interface as mod
-from log import logging
+import log
 
 
 def actions():
-    num_1, num_2, act_operation = mod.get_values()
+    act_operation, num_1, num_2 = mod.get_values()
     if act_operation == '1':
+        log.logging.info(f'{num_1} + {num_2} = {num_1 + num_2}')
         return num_1 + num_2
     elif act_operation == '2':
+        log.logging.info(f'{num_1} - {num_2} = {num_1 - num_2}')
         return num_1 - num_2
     elif act_operation == '3':
+        log.logging.info(f'{num_1} * {num_2} = {num_1 * num_2}')
         return num_1 * num_2
     elif act_operation == '4':
         if num_2 == 0:
-            logging.error("Error")
+            log.logging.error("Error")
             print("Error. It cannot be divided by 0.")
             actions()
         else:
+            log.logging.info(f'{num_1} / {num_2} = {num_1 / num_2}')
             return num_1 / num_2
     elif act_operation == '5':
-        return num_1 ** num_2
+            log.logging.info(f'{num_1} ** {num_2} = {num_1 ** num_2}')
+            return num_1 ** num_2
     elif act_operation == '6':
         if num_2 == 0:
-            logging.error("Error")
+            log.logging.error("Error")
             print("Error. Invalid argument.")
             actions()
         else:
+            log.logging.info(f'{num_1} root {num_2} = {num_1 ** (1/num_2)}')
             return num_1 ** (1/num_2)
     elif act_operation == '7':
         if num_2 == 0:
-            logging.error("Error")
+            log.logging.error("Error")
             print("Error. It cannot be divided by 0.")
             actions()
         else:
+            log.logging.info(f'{num_1} % {num_2} = {num_1 % num_2}')
             return num_1 % num_2
     elif act_operation == '8':
         if num_2 == 0:
-            logging.error("Error")
+            log.logging.error("Error")
             print("Error. It cannot be divided by 0.")
             actions()
         else:
+            log.logging.info(f'{num_1} // {num_2} = {num_1 // num_2}')
             return num_1 // num_2
     elif act_operation == '0':
         actions()
